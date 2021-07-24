7e003196cd509592879d5ad8575176bfafb8516c
levifrota <levi.frota.09@hotmail.com>
2021-07-24 16:25:41 -0300

    adicionados os arquivos referentes ao desafio #2

---
+++ pagina-produtos/src/globalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    font-family: 'Asket Narrow Light';
+    font-style: normal;
+    font-weight: normal;
+    src: local('Asket Narrow Light'), url('Asket Narrow Light.woff') format('woff');
+  }
+`;
